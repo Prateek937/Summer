805961fc952a28f56cad31dc87366d66c40ab18a
Prateek937 <prateeksanodiya937.com>
2021-06-29 11:36:51 +0530

    LSTM

---
+++ flutter/main.dart
@@ -0,0 +1,10 @@
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    )
+  }
+}
